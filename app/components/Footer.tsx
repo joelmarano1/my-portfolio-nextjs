9ae0c912035c549e5171fa601782bfb5867b5bc1
Joel Marano <joelmarano1@gmail.com>
2023-12-29 11:09:19 +0800

    push footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <section className="text-center pb-5">
-        <span className="text-white text-md"> © Designed & Developed by Joel Maraño</span>
+        <span className="text-white dark:text-[#2C3F50] text-md"> © Designed & Developed by Joel Maraño</span>
     </section>
   )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       }}>
        <nav className="flex items-center  justify-between   px-2 md:px-2 lg:px-5 2xl:px-56 relative z-30 pt-5 pb-0 lg:pb-10">
         <span className='cursor-pointer dark:text-[#2C3F50] text-white text-2xl font-bold'>
-            &lt;/dev&gt;
+            {/* &lt;/dev&gt; */}
         </span>
         <span className='cursor-pointer text-white'>
            <ThemeSwitch/>
